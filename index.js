7d6b25bbee49f98231811aa946dd457edad19cb9
Sandeep Yadawad <sandu4168@gmail.com>
2022-03-19 13:30:30 +0530

    Uploading starting files

---
+++ index.js
@@ -0,0 +1,60 @@
+
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+
+for ( let i = 0; i < numberOfDrumButtons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+
+        let key = this.innerHTML;
+        playAudio(key);
+    })
+}document.querySelector(".drum").addEventListener("keypress",function (event){
+
+        playAudio(event.key);
+
+})
+
+
+
+function playAudio(key) {
+
+    switch (key){
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+    }
+
+
+}
+
+function keyPress(key){
+    key.key
+}
+
+
+
